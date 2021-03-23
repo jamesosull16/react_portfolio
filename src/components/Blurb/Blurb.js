618480c556a47f8a506c1f50227a9d27e6e6c65d
jamesosull16 <jamesosull16@gmail.com>
2021-03-23 15:48:56 -0600

    spelling errors in blurb

--- src/components/Blurb/Blurb.js
+++ src/components/Blurb/Blurb.js
@@ -7,13 +7,13 @@ function Blurb() {
     <Segment raised>
       I am currently working as a Software Test Engineer for a gym membership
       management software company. I love the job for it's daily challenges,
-      endless growth oppurtunites but most importantly for the people and the
+      endless growth opportunities, but most importantly for the people and the
       passion they bring everyday to help make the world a healthier and happier
       place. Coming from a customer service and testing background and with
       aspirations of moving into a Software Engineer role, I write my code with
       a mobile first mindset and with the end-user at the forfront of my mind.
-      As an endurance athlete, I am constantly outside of my comfort
-      zone which pushes me to adapt and learn and be the best version of myself everyday!
+      As an endurance athlete, I am constantly outside of my comfort zone which
+      pushes me to adapt and learn and be the best version of myself everyday!
       Thank you for stopping by and please do not hesitate to reach out or
       connect with me via the various social networks available.{" "}
     </Segment>
